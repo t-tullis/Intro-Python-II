83ef0fd690e7e973bbbfa5d11fd4db2a6a9f6167
t-tullis <t.tullis34@gmail.com>
2019-05-30 15:55:21 -0700

    generate random item in room, with the ability to pick up and put into inventory

--- src/player.py
+++ src/player.py
@@ -1,9 +1,10 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
 class Player():
-    def __init__(self, name, current_room):
+    def __init__(self, name, current_room, inventory = []):
         self.name = name
         self.current_room = current_room
+        self.inventory = []
 
     def movement(self, direction):
         if(direction == 'n'):
